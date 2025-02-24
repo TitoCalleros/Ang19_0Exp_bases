bc7a7940836e699bf9afb2e8f865032e9221496e
TitoCalleros <mirna.molina@gmail.com>
2025-02-23 18:45:44 -0600

    Se agregan clases de estilos condicionales

--- src/app/pages/dragonball/dragonball-page/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page/dragonball-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 interface Character {
   id: number;
@@ -7,7 +7,7 @@ interface Character {
 }
 
 @Component({
-  imports: [],
+  imports: [ ],
   templateUrl: './dragonball-page.component.html',
   styles: ``
 })
@@ -16,7 +16,14 @@ export class DragonballPageComponent {
   public characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
-    { id: 2, name: 'Piccolo', power: 3500 }
+    { id: 3, name: 'Piccolo', power: 3500 },
+    { id: 4, name: 'Yamcha', power: 500 }
   ]);
+
+  powerClasses = computed( () => {
+    return {
+      'text-danger': true,
+    }
+  } );
 
 }
